f547226335efafe9433f15e98ebfa2d1eb239ffd
Jingxin-Li <18686030374@163.com>
2020-03-18 16:53:00 +0800

    init repo

---
+++ src/main/java/top/lijingxin/blog/controller/HelloController.java
@@ -0,0 +1,19 @@
+package top.lijingxin.blog.controller;
+
+import com.sun.xml.internal.ws.api.server.Module;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Ali
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name",defaultValue = "null")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
