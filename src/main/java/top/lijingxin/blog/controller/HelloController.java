a5ea2b2966ede00f7f3c3c57c370e5f3e06b9e37
Jingxin-Li <18686030374@163.com>
2020-03-18 18:22:10 +0800

    add index

--- src/main/java/top/lijingxin/blog/controller/HelloController.java
+++ src/main/java/top/lijingxin/blog/controller/HelloController.java
@@ -1,6 +1,5 @@
 package top.lijingxin.blog.controller;
 
-import com.sun.xml.internal.ws.api.server.Module;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name",defaultValue = "null")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
